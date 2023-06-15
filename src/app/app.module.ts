3b3f49bcf100eabf517f22b477e5024993bf6e25
Vincent <comeyne33@gmail.com>
2023-06-15 18:03:12 +0200

    svgadd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -14,7 +15,8 @@ import { Component2Component } from './component2/component2.component';
     Component2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component1/component1.component.ts
+++ src/app/component1/component1.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-component1',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./component1.component.css']
 })
 export class Component1Component {
+  imageUrl!: string;
+  error: any;
 
+  constructor(private http: HttpClient) {
+    this.getImageUrl();
+  }
+
+  getImageUrl() {
+    this.http.get('/get_bar', { responseType: 'text' }).subscribe(
+      data => {
+        this.imageUrl = data;
+      },
+      (error: HttpErrorResponse) => {
+        this.error = error;
+        console.error('Er was een fout bij het ophalen van de gegevens:', error);
+      }
+    );
+  }
 }
