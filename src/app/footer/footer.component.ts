76e30213182d39afec769ee40705b3407447b351
Vincent <comeyne33@gmail.com>
2023-06-16 12:44:05 +0200

    footer+href

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  month: string;
 
+  constructor() {
+    this.month = this.getCurrentMonth();
+  }
+
+  getCurrentMonth(): string {
+    const monthNames = ["Januari", "Februari", "Maart", "April", "Mei", "Juni", "Juli", "Augustus", "September", "Oktober", "November", "December"];
+    const currentDate = new Date();
+    return monthNames[currentDate.getMonth()];
+  }
 }
